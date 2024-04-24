e560bffe5e0edff038d02cf1283c0d2b5f1124ff
vishvajit2856 <124611965+vishvajit2856@users.noreply.github.com>
2024-04-24 08:49:30 -0500

    Update Home_page.js

--- Home_page.js
+++ Home_page.js
@@ -1,12 +1,16 @@
 function call_next_html_file(){
     
-    // window.location.href="template_selection.html";
-    var newWindow = window.open("template_selection.html", "_blank");
-    if (newWindow) {
-      newWindow.document.body.style.zoom = "80%";
-    }
-    
-    
+    //window.location.href="template_selection.html";
+   // var newWindow = window.open("template_selection.html", "_blank");
+  
+    document.body.style.opacity = 0;
+    setTimeout(function() {
+        window.location.href = "template_selection.html";
+    }, 500);
+
+
+
+
     // document.body.style.zoom = "80%";    
 }
 
